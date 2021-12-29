8d0940060d894b4b677d02b5c2e11d6ed2d9efa8
Raul Romero Cano <raulromerocano@Rauls-MacBook-Air.local>
2021-12-29 16:18:04 -0600

    Imported data.js to App.js. Raw values are displayed as a table.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import data from './data'
 
 const App = () => (
   <div className="app">
@@ -10,6 +11,19 @@ const App = () => (
     <p>
       Welcome to the app!
     </p>
+    <table>
+      <tbody>
+        {data.routes.map(route => {
+          return (
+            <tr>
+              <td>{route.airline}</td>
+              <td>{route.src}</td>
+              <td>{route.dest}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
   </section>
 </div>
 )
